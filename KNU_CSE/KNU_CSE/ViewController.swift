68941fdd97fa49ace9d4cdb253360efae95abfc2
junseokNoh <ljs3271@naver.com>
2021-07-15 15:09:58 +0900

    project init

---
+++ KNU_CSE/KNU_CSE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KNU_CSE
+//
+//  Created by junseok on 2021/07/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KNU_CSE/KNU_CSETests/KNU_CSETests.swift
@@ -0,0 +1,33 @@
+//
+//  KNU_CSETests.swift
+//  KNU_CSETests
+//
+//  Created by junseok on 2021/07/15.
+//
+
+import XCTest
+@testable import KNU_CSE
+
+class KNU_CSETests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
