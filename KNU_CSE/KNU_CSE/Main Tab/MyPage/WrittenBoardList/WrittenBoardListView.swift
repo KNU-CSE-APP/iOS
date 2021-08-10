9d402cae092144f19377f8c2cc3b68ba31c22e5a
junseokNoh <ljs3271@naver.com>
2021-08-11 00:59:21 +0900

    #3 Edit WrittenBoardListView

--- KNU_CSE/KNU_CSE/Main Tab/MyPage/WrittenBoardList/WrittenBoardListView.swift
+++ KNU_CSE/KNU_CSE/Main Tab/MyPage/WrittenBoardList/WrittenBoardListView.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class WrittenBaordListView:UIViewController, ViewProtocol{
     
+    var BoardVC : BoardView!{
+        didSet{
+            self.BoardVC.parentType = .Write
+        }
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         self.setNavigationTitle(title: "내가 쓴 글")
         self.hideBackTitle()
@@ -20,7 +26,7 @@ class WrittenBaordListView:UIViewController, ViewProtocol{
     }
     
     func initUI() {
-        
+        self.BoardVC = self.storyboard?.instantiateViewController(withIdentifier: "BoardView") as? BoardView
     }
     
     func addView() {
@@ -35,14 +41,11 @@ class WrittenBaordListView:UIViewController, ViewProtocol{
 extension WrittenBaordListView{
     
     func addBoardView(){
-        guard let BoardVC = storyboard?.instantiateViewController(withIdentifier: "BoardView") as? BoardView else{
-            return
-        }
-        self.addChild(BoardVC)
-        self.view.addSubview(BoardVC.view)
-        BoardVC.didMove(toParent: self)
-        BoardVC.view.backgroundColor = .yellow
-        BoardVC.view.snp.makeConstraints{ make in
+        self.addChild(self.BoardVC)
+        self.view.addSubview(self.BoardVC.view)
+        self.BoardVC.didMove(toParent: self)
+        self.BoardVC.view.backgroundColor = .yellow
+        self.BoardVC.view.snp.makeConstraints{ make in
             make.top.equalTo(self.view.safeAreaLayoutGuide)
             make.left.equalTo(self.view.safeAreaLayoutGuide)
             make.right.equalTo(self.view.safeAreaLayoutGuide)
